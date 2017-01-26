85054f62006855da63f4bfcfb26062d9893ea3fc
e154941 <e154941@E-0621.univ-lemans.fr>
2017-01-26 12:36:50 +0100

    init contolleur

--- Sudo Assit/src/controller/ControlleurPagePrincipale.java
+++ Sudo Assit/src/controller/ControlleurPagePrincipale.java
@@ -1,5 +1,17 @@
 package controller;
 
-public class ControlleurPagePrincipale {
+import java.io.IOException;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControlleurPagePrincipale {
+	
+	@FXML private Button plus;
+	
+	@FXML
+	protected void doPlus(ActionEvent event) throws IOException {
+		 
+	}
 }
