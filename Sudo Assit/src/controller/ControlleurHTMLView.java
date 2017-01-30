7b571212b14d1714eb82ba75ba11c065cd834b28
YvesTAGNY <yves.tagny@yahoo.fr>
2017-01-30 12:52:07 +0100

    chargement de pages html

--- Sudo Assit/src/controller/ControlleurHTMLView.java
+++ Sudo Assit/src/controller/ControlleurHTMLView.java
@@ -7,12 +7,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.web.WebView;
 
-public class ControlleurRDJ  implements Initializable {
+public class ControlleurHTMLView  implements Initializable {
 	
 	   @FXML private WebView webView;
 	   
 		public void initialize(URL location, ResourceBundle resources) {
 			// TODO Auto-generated method stub
-			webView.getEngine().load("file:///C:/Users/TAGNY/git/SudoAssit/Sudo%20Assit/src/resources/rdj.htm");
+			final URL url = getClass().getResource("/resources/rdj.htm"); 
+			webView.getEngine().load(url.toExternalForm());
 		}
 }
